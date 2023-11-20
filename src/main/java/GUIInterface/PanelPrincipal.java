61185824b24cb8ff4b7b69d5ad988e8d24bc0d02
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-11-20 14:43:39 -0300

    Se agrega ActionListener en PanelPrincipal
    se encarga de cambiar de DestinoIda hacia horarios.

--- src/main/java/GUIInterface/PanelPrincipal.java
+++ src/main/java/GUIInterface/PanelPrincipal.java
@@ -2,6 +2,8 @@ package GUIInterface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -17,6 +19,14 @@ public class PanelPrincipal extends JPanel implements MouseListener {
 
         destinoIda = new PanelDestinoIda();
         destinoIda.setVisible(false);
+        ActionListener horaselec = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                destinoIda.setVisible(false);
+                horarios.setVisible(true);
+            }
+        };
+        destinoIda.getOrigen_Destino().addActionListener(horaselec);
 
         horarios = new PanelHorarios();
         horarios.setVisible(false);
@@ -24,6 +34,8 @@ public class PanelPrincipal extends JPanel implements MouseListener {
         compra.getComprarAsiento().addMouseListener(this);
 
         setPreferredSize(new Dimension(1920,1080));
+
+
     }
 
     @Override
@@ -35,21 +47,26 @@ public class PanelPrincipal extends JPanel implements MouseListener {
 
         destinoIda.setLocation(0,0);
         compra.setLocation(0,0);
+        horarios.setLocation(0,0);
+
         if(compra.isVisible()){
             compra.paintComponent(g);
         }
+
         else if(destinoIda.isVisible()){
             destinoIda.paintComponent(g);
         }
+
         else if(horarios.isVisible()){
-            horarios.paintComponents(g);
+            horarios.paintComponent(g);
         }
+
     }
     @Override
     public void mouseClicked(MouseEvent e) {
         if(e.getComponent()==compra.getComprarAsiento()){
-            compra.setVisible(!compra.isVisible());
-            destinoIda.setVisible(!destinoIda.isVisible());
+            compra.setVisible(false);
+            destinoIda.setVisible(true);
             repaint();
         }
     }
@@ -66,16 +83,10 @@ public class PanelPrincipal extends JPanel implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        if(e.getComponent()==compra) {
-            System.out.println("dentro");
-        }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        if(e.getComponent()==compra) {
-            System.out.println("fuera");
-        }
     }
 
     public static void main (String[] args){
@@ -85,8 +96,9 @@ public class PanelPrincipal extends JPanel implements MouseListener {
         frame.add(panelPrincipal);
         frame.setTitle("Venta de pasajes");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(1000,1000);
+        frame.setSize(1500,1020);
         frame.setVisible(true);
     }
+
 
 }
