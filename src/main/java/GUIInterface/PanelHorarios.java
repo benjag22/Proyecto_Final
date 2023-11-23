6f0e71478e147e948af64a5caabfa7478b798ce6
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-11-23 20:48:06 -0300

    Se agregan buses a PanelHorarios con sus horas de viaje y Origen/Destino respectivos

--- src/main/java/GUIInterface/PanelHorarios.java
+++ src/main/java/GUIInterface/PanelHorarios.java
@@ -1,5 +1,10 @@
 package GUIInterface;
 
+import org.example.Bus;
+import org.example.BusDosPisos;
+import org.example.BusUnPiso;
+import org.example.Horario;
+
 import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
 import javax.swing.*;
@@ -9,6 +14,7 @@ import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalTime;
 import javax.sound.sampled.*;
 
 public class PanelHorarios extends JPanel implements ItemListener {
@@ -18,11 +24,48 @@ public class PanelHorarios extends JPanel implements ItemListener {
     private Clip clipMouseOver;
     private Clip clipClick;
     private JComboBox horarios;
+    private Bus bus1;
+    private Bus bus2;
+    private Bus bus3;
+    private Bus bus4;
+    private Bus bus5;
+    private Bus bus6;
+    private Bus bus7;
+    private Bus bus8;
+
 
 public PanelHorarios(){
     this.setLayout(null);
     setPreferredSize(new Dimension(1920,1080));
     this.imagen =cargarImagen("");
+    bus1= new BusUnPiso();
+    bus2= new BusUnPiso();
+    bus3= new BusUnPiso();
+    bus4= new BusUnPiso();
+
+    bus5= new BusDosPisos();
+    bus6= new BusDosPisos();
+    bus7= new BusDosPisos();
+    bus8= new BusDosPisos();
+
+    bus1.setHorario(new Horario(LocalTime.of(9,0),LocalTime.of(10,45)));
+    bus1.setOrigen_Destino("CONC/LA");
+    bus2.setHorario(new Horario(LocalTime.of(11,0),LocalTime.of(12,45)));
+    bus2.setOrigen_Destino("LA/CONC");
+    bus3.setHorario(new Horario(LocalTime.of(13,0),LocalTime.of(14,45)));
+    bus3.setOrigen_Destino("CONC/LA");
+    bus4.setHorario(new Horario(LocalTime.of(15,0),LocalTime.of(16,45)));
+    bus4.setOrigen_Destino("LA/CONC");
+    bus5.setHorario(new Horario(LocalTime.of(17,0),LocalTime.of(18,45)));
+    bus5.setOrigen_Destino("CONC/LA");
+    bus6.setHorario(new Horario(LocalTime.of(19,0),LocalTime.of(20,45)));
+    bus6.setOrigen_Destino("LA/CONC");
+    bus7.setHorario(new Horario(LocalTime.of(21,0),LocalTime.of(22,45)));
+    bus7.setOrigen_Destino("CONC/LA");
+    bus8.setHorario(new Horario(LocalTime.of(23,0),LocalTime.of(0,45)));
+    bus8.setOrigen_Destino("LA/CONC");
+
+
 
     Font font = new Font("Arial",Font.PLAIN,30);
     Seleccionar = new JLabel("Seleccionar Horario");
@@ -34,14 +77,14 @@ public PanelHorarios(){
     horarios = new JComboBox();
     horarios.addItemListener(this);
     horarios.addItem("Eliga su horario");
-    horarios.addItem("10:00");
-    horarios.addItem("11:00");
-    horarios.addItem("12:00");
-    horarios.addItem("13:00");
-    horarios.addItem("14:00");
-    horarios.addItem("15:00");
-    horarios.addItem("16:00");
-    horarios.addItem("17:00");
+    horarios.addItem(bus1.getHorario());
+    horarios.addItem(bus2.getHorario());
+    horarios.addItem(bus3.getHorario());
+    horarios.addItem(bus4.getHorario());
+    horarios.addItem(bus5.getHorario());
+    horarios.addItem(bus6.getHorario());
+    horarios.addItem(bus7.getHorario());
+    horarios.addItem(bus8.getHorario());
 
     horarios.setBounds(400,300,300,50);
     add(horarios);
@@ -91,5 +134,37 @@ public PanelHorarios(){
             clip.setMicrosecondPosition(0);
             clip.start();
         }
+    }
+
+    public Bus getBus1() {
+        return bus1;
+    }
+
+    public Bus getBus2() {
+        return bus2;
+    }
+
+    public Bus getBus3() {
+        return bus3;
+    }
+
+    public Bus getBus4() {
+        return bus4;
+    }
+
+    public Bus getBus5() {
+        return bus5;
+    }
+
+    public Bus getBus6() {
+        return bus6;
+    }
+
+    public Bus getBus7() {
+        return bus7;
+    }
+
+    public Bus getBus8() {
+        return bus8;
     }
 }
