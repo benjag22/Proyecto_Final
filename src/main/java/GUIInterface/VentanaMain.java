e1a0fcabdd33450cb1b12756126691f232444fe0
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-11-23 20:48:25 -0300

    se agrega PanelPrincipal

--- src/main/java/GUIInterface/VentanaMain.java
+++ src/main/java/GUIInterface/VentanaMain.java
@@ -6,6 +6,7 @@ public class VentanaMain extends JFrame {
     public static void main (String[] args){
         JFrame frame = new JFrame();
         PanelPrincipal panelPrincipal = new PanelPrincipal();
+        frame.add(panelPrincipal);
         frame.setTitle("Venta de pasajes");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(1000,1000);
