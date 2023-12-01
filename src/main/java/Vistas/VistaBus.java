7915b31396d1e4e3db736bd38d9eff1edfccc112
benjag22 <benjagonzalez2022@udec.cl>
2023-12-01 00:46:59 -0300

    Upgrade De vistaBus

--- src/main/java/Vistas/VistaBus.java
+++ src/main/java/Vistas/VistaBus.java
@@ -1,27 +1,46 @@
 package Vistas;
-import javax.swing.*;
 import org.example.*;
+
+import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class VistaBus extends JPanel {
+
     private Bus bus;
-    private JButton btnPaginaAnterior;
-    private JButton btnPaginaSiguiente;
+    private JPanel seatingArea;
+    private CardLayout cardLayout;
     private ArrayList<VistasAsientos> listaAsientos;
     private int paginaActual = 1;
 
     public VistaBus(Bus bus) {
-        super();
         this.bus = bus;
         listaAsientos = new ArrayList<>();
+        seatingArea = new JPanel();
+        cardLayout = new CardLayout();
+        seatingArea.setLayout(cardLayout);
+
         if (bus.getPisos() == 2) {
-            btnPaginaAnterior = new JButton("<< Anterior");
-            btnPaginaSiguiente = new JButton("Siguiente >>");
+            agregarPagina(1);
+            agregarPagina(2);
+            cardLayout.show(seatingArea, "1");
+
+            JButton btnPaginaAnterior = new JButton("<< Anterior");
             btnPaginaAnterior.addActionListener(e -> cambiarPagina(-1));
+
+            JButton btnPaginaSiguiente = new JButton("Siguiente >>");
             btnPaginaSiguiente.addActionListener(e -> cambiarPagina(1));
+
+            JPanel navigationPanel = new JPanel();
+            navigationPanel.add(btnPaginaAnterior);
+            navigationPanel.add(btnPaginaSiguiente);
+
+            setLayout(new BorderLayout());
+            add(seatingArea, BorderLayout.CENTER);
+            add(navigationPanel, BorderLayout.SOUTH);
         }
     }
+
     private void cambiarPagina(int delta) {
         paginaActual += delta;
         if (paginaActual < 1) {
@@ -29,72 +48,71 @@ public class VistaBus extends JPanel {
         } else if (paginaActual > 2) {
             paginaActual = 2;
         }
-        limpiarAsientos();
-        agregarAsientos(new AsientoCama("A", 1), paginaActual);
+        cardLayout.show(seatingArea, String.valueOf(paginaActual));
     }
 
-    private void limpiarAsientos() {
-        removeAll();
-        listaAsientos.clear();
+    private void agregarPagina(int pagina) {
+        JPanel paginaPanel = new JPanel(new BorderLayout());
+        JPanel asientoPanel = new JPanel(new GridLayout());
+        agregarAsientos(new AsientoCama("A", 1), pagina, asientoPanel);
+        JScrollPane scrollPane = new JScrollPane(asientoPanel);
+        paginaPanel.add(scrollPane, BorderLayout.CENTER);
+        paginaPanel.setName(String.valueOf(pagina));
+        seatingArea.add(paginaPanel, String.valueOf(pagina));
+        cardLayout.show(seatingArea, String.valueOf(pagina));
     }
 
-   @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-
+    private void agregarAsientos(Asiento asiento, int cualCorridaAsiento,JPanel pagina) {
+        int filas = obtenerFilas(cualCorridaAsiento);
+        int columnas = obtenerColumnas(cualCorridaAsiento);
+        int xposicion = obtenerXPosicion(asiento);
+        int yposicion = obtenerYPosicion(asiento);
+        for (int i = 1; i <= filas; i++) {
+            for (int j = 1; j <= columnas; j++) {
+                char letra = (char) ('A' + i - 1);
+                int x = xposicion * (j - 1);
+                int y = yposicion * (i - 1);
+                Asiento nuevoAsiento = crearNuevoAsiento(asiento, letra,j);
+                VistasAsientos vistanuevoAsiento = new VistasAsientos(nuevoAsiento);
+                vistanuevoAsiento.setLocation(x, y);
+                pagina.add(vistanuevoAsiento);
+                listaAsientos.add(vistanuevoAsiento);
+            }
+        }
     }
-   public void agregarAsientos(Asiento asiento, int cualCorridaAsiento) {
-       int filas=0;
-       int columnas=0;
-       int xposicion;
-       int yposicion;
-           if (cualCorridaAsiento == 1) {
-               filas = CantidadesAsientoPisos.PISONORMAL.getFILAS();
-               columnas = CantidadesAsientoPisos.PISONORMAL.getCOLUMNAS();
-           } else if (cualCorridaAsiento == 2) {
-               filas = CantidadesAsientoPisos.PISOREDUCIDO.getFILAS();
-               columnas = CantidadesAsientoPisos.PISOREDUCIDO.getCOLUMNAS();
-           }
-           if(asiento instanceof AsientoCama){
-               xposicion=90;
-               yposicion=60;
-           }else{
-               xposicion=70;
-               yposicion=70;
-           }
-       for (int i = 1; i <= filas; i++) {
-           for (int j = 1; j <= columnas; j++) {
-               char letra = (char) ('A' + i - 1);
-               int x= xposicion*(i - 1);
-               int y= yposicion*(j - 1);
-               Asiento nuevoAsiento;
-               if (asiento instanceof AsientoCama) {
-                   nuevoAsiento = new AsientoCama(String.valueOf(letra), j);
-               } else {
-                   nuevoAsiento = new AsientoSemiCama(String.valueOf(letra), j);
-               }
-               VistasAsientos vistanuevoAsiento = new VistasAsientos(nuevoAsiento);
-               vistanuevoAsiento.setLocation(x, y);
-               add(vistanuevoAsiento);
-               listaAsientos.add(vistanuevoAsiento);
-               }
-           }
-       }
+
+
+    private int obtenerFilas(int cualCorridaAsiento) {
+        return (cualCorridaAsiento == 1) ? CantidadesAsientoPisos.PISONORMAL.getFILAS() : CantidadesAsientoPisos.PISOREDUCIDO.getFILAS();
+    }
+
+    private int obtenerColumnas(int cualCorridaAsiento) {
+        return (cualCorridaAsiento == 1) ? CantidadesAsientoPisos.PISONORMAL.getCOLUMNAS() : CantidadesAsientoPisos.PISOREDUCIDO.getCOLUMNAS();
+    }
+
+    private int obtenerXPosicion(Asiento asiento) {
+        return (asiento instanceof AsientoCama) ? 90 : 70;
+    }
+
+    private int obtenerYPosicion(Asiento asiento) {
+        return (asiento instanceof AsientoCama) ? 70 : 70;
+    }
+
+    private Asiento crearNuevoAsiento(Asiento asiento, char letra, int numero) {
+        return (asiento instanceof AsientoCama) ? new AsientoCama(String.valueOf(letra), numero) : new AsientoSemiCama(String.valueOf(letra), numero);
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            BusDosPisos bus= new BusDosPisos();
-            VistaBus vistaBus = new VistaBus(bus);
-            vistaBus.agregarAsientos(new AsientoCama("A", 1), 2);
+            BusDosPisos bus = new BusDosPisos();
             JFrame frame = new JFrame("VistaBus Test");
-            frame.setSize(1920, 1080);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            JPanel mainPanel = new JPanel(new BorderLayout());
-            mainPanel.add(vistaBus);
-            frame.setContentPane(mainPanel);
+            frame.setContentPane(new VistaBus(bus));
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
 }
+
 
